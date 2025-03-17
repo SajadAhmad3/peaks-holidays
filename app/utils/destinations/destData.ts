e08caa671107c05a10800617d12695886f95c430
Sajad Ahmad <sajad.ahmad@geekyants.com>
2025-03-17 16:02:59 +0530

    fix: images

--- app/utils/destinations/destData.ts
+++ app/utils/destinations/destData.ts
@@ -47,12 +47,61 @@ export const destinations = [
     slug: "ladakh",
     destinationName: "Ladakh",
     image: "/images/home/ladakh-ride.jpg",
-    description: "Known as the 'Meadow of Gold', offering stunning views.",
+    description: "A high-altitude desert known for its dramatic landscapes.",
     activities: [
-      "Thajiwas Glacier Visit",
-      "Camping",
-      "Horse Riding",
-      "Fishing",
+      "Bike Riding",
+      "Monastery Visits",
+      "Pangong Lake Tour",
+      "River Rafting",
     ],
+  },
+  {
+    slug: "srinagar",
+    destinationName: "Srinagar",
+    image: "/images/packages/kashmir_dal.jpg",
+    description:
+      "The summer capital of Jammu and Kashmir, famous for its lakes and gardens.",
+    activities: [
+      "Shikara Ride",
+      "Houseboat Stay",
+      "Mughal Gardens Tour",
+      "Shopping",
+    ],
+  },
+  {
+    slug: "betaab-valley",
+    destinationName: "Betaab Valley",
+    image: "/images/packages/green_medows.jpg",
+    description:
+      "A picturesque valley known for its scenic beauty and Bollywood connections.",
+    activities: ["Photography", "Picnicking", "Trekking", "Horse Riding"],
+  },
+  {
+    slug: "doodhpathri",
+    destinationName: "Doodhpathri",
+    image: "/images/packages/doodhpathri.jpg",
+    description: "A hidden valley with lush green pastures and streams.",
+    activities: ["Trekking", "Picnicking", "Camping", "Photography"],
+  },
+  {
+    slug: "tulip-garden",
+    destinationName: "Indira Gandhi Tulip Garden",
+    image: "/images/packages/tulip-garden.jpg",
+    description:
+      "Asia’s largest tulip garden, offering a colorful floral paradise.",
+    activities: [
+      "Flower Viewing",
+      "Photography",
+      "Picnicking",
+      "Leisure Walks",
+    ],
+  },
+  {
+    slug: "gurez-valley",
+    destinationName: "Gurez Valley",
+    image: "/images/packages/gurez.jpg",
+    description:
+      "An offbeat destination with untouched natural beauty and scenic villages.",
+    activities: ["Trekking", "Camping", "Photography", "Cultural Exploration"],
   },
 ];
